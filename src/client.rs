bebc29fad3883cf5f6fccd66f634e3ec66451d8d
SpieringsAE <maud_spierings@hotmail.com>
2024-01-28 20:09:13 +0100

    sync commit

--- src/client.rs
+++ src/client.rs
@@ -1,3 +1,5 @@
+use std::os::unix::net::SocketAddr;
+
 use crate::tcp_client::{TCPClient, TCPConnectionParams};
 
 pub enum XCPConnectionType {
@@ -17,6 +19,19 @@ impl XCPClient {
             _ => unreachable!(),
         }
     }
+}
+
+pub struct XCPServerResponse {
+    addr: SocketAddr,
+    udp: bool,
+    tcp: bool,
+    connected: bool,
+}
+
+pub fn discover_xcp_servers() -> std::io::Result<Vec<XCPServerResponse>> {
+    let socket = std::net::UdpSocket::bind("0.0.0.0:5555")?;
+    socket.set_broadcast(true)?;
+    todo!("GET_SLAVE_ID?");
 }
 
 pub trait XCP {}
